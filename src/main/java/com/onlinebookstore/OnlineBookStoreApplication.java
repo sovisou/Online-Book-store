c7cc51b29825dc88492bf875a07e5b868dd48f05
Margaryta <marharytayarmoliuk@gmail.com>
2024-10-01 03:32:42 +0300

    First commit

--- src/main/java/com/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/com/onlinebookstore/OnlineBookStoreApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineBookStoreApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookStoreApplication.class, args);
+    }
 }
