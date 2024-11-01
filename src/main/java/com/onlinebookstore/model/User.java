6c68bc3146657c3e1d9b3f764f8e9f0116ab42b3
Margaryta <marharytayarmoliuk@gmail.com>
2024-11-01 15:38:41 +0200

    Fixed problems

--- src/main/java/com/onlinebookstore/model/User.java
+++ src/main/java/com/onlinebookstore/model/User.java
@@ -14,6 +14,8 @@ import java.util.HashSet;
 import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -21,6 +23,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Table(name = "users")
 @Getter
 @Setter
+@SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted=false")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
